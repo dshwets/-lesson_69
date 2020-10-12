5ce07f97a4281f39671df9eb4e0ff99118235623
Daniil <dshwets@gmail.com>
2020-10-13 00:29:37 +0600

    Первое задание выполненно.

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from api_v1.views import get_token_view, ArticleCreateView
+from api_v1.views import get_token_view, ArticleCreateView, AddView
 
 app_name = 'api_v1'
 
 urlpatterns = [
     path('get-token/', get_token_view, name='get_token'),
-    path('article-create/', ArticleCreateView.as_view(), name='article_create')
+    path('article-create/', ArticleCreateView.as_view(), name='article_create'),
+    path('add/', AddView.as_view(), name='add'),
+    path('subtract/', AddView.as_view(), name='subtract'),
+    path('multiply/', AddView.as_view(), name='multiply'),
+    path('divide/', AddView.as_view(), name='divide'),
 ]
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,5 +1,5 @@
 import json
-from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse, HttpResponseBadRequest
 from django.views.generic import View
 from django.views.decorators.csrf import ensure_csrf_cookie
 
@@ -30,3 +30,26 @@ class ArticleCreateView(View):
             'created_at': article.created_at.strftime('%Y-%m-%d %H:%M:%S'),
             'updated_at': article.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
         })
+
+
+class AddView(View):
+    def post(self,request,*args,**kwargs):
+        data = json.loads(request.body)
+        try:
+            data['A'] = float(data['A'])
+            data['B'] = float(data['B'])
+            if request.path.split('/')[3] == 'add':
+                answer = data['A'] + data['B']
+            elif request.path.split('/')[3] == 'subtract':
+                answer = data['A'] - data['B']
+            elif request.path.split('/')[3] == 'multiply':
+                answer = data['A'] * data['B']
+            elif request.path.split('/')[3] == 'divide':
+                answer = data['A'] / data['B']
+            else:
+                answer = None
+            return JsonResponse({
+                "answer": answer
+            })
+        except Exception as e:
+            return HttpResponseBadRequest(str(e))
